1822f1d05110af6542ee799a1fd6614d314aecb0
saibabaoi <saibaba.kalagotla@orbitalinsight.com>
2021-09-25 22:38:28 +0530

    feat :: adding antd support

--- car-app-ui/src/pages/Home/index.tsx
+++ car-app-ui/src/pages/Home/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import { Col, Row, Spin} from 'antd'
+import {chunk} from 'lodash'
 import {useQuery} from '@apollo/client'
 import {GET_COMPANIES} from '../../queries'
+import PictureCard from '../../components/PictureCard'
 
-interface Car  {
-    id: number,
-    Name: string
-}
 interface Company {
     id: number,
     Name: string,
@@ -15,16 +14,25 @@ interface Company {
 
 export default function Home():JSX.Element {
     const { loading, data } = useQuery<Record<"companies", [Company]>>(GET_COMPANIES)
+    const chunks = chunk(data?.companies || [], 3)
     return (
         <div>
             {
                 (loading || !data)
-                    ? <h1>Loading...</h1>
-                    : data.companies.map(r => (
-                    <div>
-                        {r.id}-{r.Name}
-                        <img src={r.logo} />
-                    </div>
+                    ? <h1><Spin /></h1>
+                    : chunks.map((row: Company[]) => (
+                        <Row  gutter={16}>
+                            {
+                                row.map(c => (
+                                   <Col span={8}>
+                                      <PictureCard
+                                        imageUrl={c.logo}
+                                        title={c.Name}
+                                        />
+                                   </Col> 
+                                ))
+                            }
+                        </Row>
                     ))
             }
         </div>
